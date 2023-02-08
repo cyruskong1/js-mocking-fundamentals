801203ff8e8d451bc5ba540634068be129a41dfb
cy kong <cyrus@bstock.com>
2023-02-08 12:14:39 -0600

    add notes

--- src/no-framework/spy.js
+++ src/no-framework/spy.js
@@ -2,6 +2,10 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+// the fn function is a factory
+// returns a mockFn object
+// mockFn accepts args and pushes it into a calls array inside a mock property
+// mockFn returns the implementation called with args 
 function fn(impl = () => {}) {
   const mockFn = (...args) => {
     mockFn.mock.calls.push(args)
@@ -12,16 +16,21 @@ function fn(impl = () => {}) {
   return mockFn
 }
 
+// spyOn is in charge of identifying and remembering an implementation.
+// should assign an implementation to a mock implementation 
+// run the mock implementation
+// then restore it to its original value
 function spyOn(obj, prop) {
   const originalValue = obj[prop]
   obj[prop] = fn()
-  obj[prop].mockRestore = () => (obj[prop] = originalValue)
+  obj[prop].mockRestore = () => obj[prop] = originalValue
 }
 
-spyOn(utils, 'getWinner')
-utils.getWinner.mockImplementation((p1, p2) => p1)
+spyOn(utils, "getWinner")
+utils.getWinner.mockImplementation = fn((p1, p2) =>  p2)
 
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
+
 assert.strictEqual(winner, 'Kent C. Dodds')
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
   ['Kent C. Dodds', 'Ken Wheeler'],
